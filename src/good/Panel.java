d4ef133c5ab2c381aee414499f0ac77734a5330a
Lane Schwartz <dowobeha@gmail.com>
2010-05-25 16:52:31 +0000

    More work
    git-svn-id: https://joshua.svn.sourceforge.net/svnroot/joshua/branches/lane.scratch@1668 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

--- src/good/Panel.java
+++ src/good/Panel.java
@@ -2,19 +2,12 @@ package good;
 
 
 import java.awt.Container;
-import java.awt.GraphicsEnvironment;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.Point;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
-import javax.swing.BoxLayout;
 import javax.swing.JComboBox;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -38,7 +31,6 @@ public class Panel extends JPanel {
 		
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-//		int emptyScrollPaneHeight = scrollPane.getHeight();
 		
 		parent.add(scrollPane);
 		
@@ -51,7 +43,6 @@ public class Panel extends JPanel {
 			c.weightx = 1;
 			c.fill = GridBagConstraints.HORIZONTAL;
 			JLabel label = new JLabel("");
-			//			label.setBorder(BorderFactory.createLineBorder(Color.black));
 			this.add(label,c);
 		}
 
@@ -64,15 +55,10 @@ public class Panel extends JPanel {
 			c.gridwidth = 2;
 			c.weightx = 1;
 			c.fill = GridBagConstraints.HORIZONTAL;
-			//			System.err.println(c.gridx);
+
 			JLabel label = new JLabel(args[i], JLabel.CENTER);
-//			label.addMouseListener(listener);
-			//			label.setBorder(BorderFactory.createLineBorder(Color.black));
 			this.add(label,c);
 			this.doLayout();
-//			parent.setVisible(true);
-			//System.err.println("label["+i+"] is at ("+label.getX()+","+label.getY()+") - ("+(label.getX()+label.getWidth())+","+(label.getY()+label.getHeight())+")");
-//			
 		}
 		
 		JComboBox[][] comboBoxes;
@@ -96,7 +82,6 @@ public class Panel extends JPanel {
 					c.gridwidth = 2;
 					c.weightx = 1;
 					c.fill = GridBagConstraints.HORIZONTAL;
-					//				System.err.println(c.gridx);
 
 					StringBuilder sourcePhraseBuilder = new StringBuilder();
 					for (int wordIndex=cell, lastIndex=cell+row; wordIndex<=lastIndex; wordIndex++) {
@@ -108,120 +93,16 @@ public class Panel extends JPanel {
 					Model model = new Model(sourcePhraseBuilder.toString(), translationsList);
 					JComboBox comboBox = new JComboBox(model);
 					comboBox.setEditable(true);
-//					comboBox.addActionListener(l);
 					comboBoxes[row][cell] = comboBox;
-//					for (Component component : comboBox.getComponents()) {
-//						component.addMouseListener(listener);
-//					}
-					//				comboBox.addMouseListener(listener);
 					this.add(comboBox,c);
 					this.doLayout();
-//					window.doLayout();
-//					window.validate();
 					parent.setVisible(true);
-					
-//					Component component = comboBox.getEditor().getEditorComponent();
-					//System.err.println("comboBox["+row+"]["+cell+"] is at ("+comboBox.getX()+","+comboBox.getY()+") - ("+(comboBox.getX()+comboBox.getWidth())+","+(comboBox.getY()+comboBox.getHeight())+")");
-//					System.err.println("comboBox["+row+"]["+cell+"] is at + " + comboBox.getVisibleRect());
-					
-					//comboBox.setEnabled(false);
-					//				JLabel label = new JLabel(" R" + row + ",C"+cell);
-					//				label.setBorder(BorderFactory.createLineBorder(Color.black));
-					//				pane.add(label,c);
 				}
 
 			}
 
-			
-			
 		}
-		
-		
-		
-//		int height = emptyScrollPaneHeight + (int) parent.getMinimumSize().getHeight();
-//		int width = (int) scrollPane.getPreferredSize().getWidth();
-//		Dimension size = new Dimension(width, height);
-//		
-//		scrollPane.setPreferredSize(size);
-//		scrollPane.setSize(size);
 		
 	}
-	
-	
-	public static void main(String[] args) throws IOException, ClassNotFoundException {
-	
-		String joshDir = "/Users/lane/Research/brainstorm/Joshua Scratch/data/europarl.es-en.10000.josh";
-		SourceText sourceText = new SourceText("/Users/lane/Research/brainstorm/Joshua Scratch/data/europarl.es.10");
-		
-		BinaryParallelCorpus corpus = new BinaryParallelCorpus(joshDir, sourceText);
-		
-		TranslationOptions translations = new TranslationOptions(corpus);
-		
-		int spanLimit = 5;
-		
-		JFrame window = new JFrame();
-		JPanel parent = new JPanel();
-		parent.setLayout(new BoxLayout(parent, BoxLayout.PAGE_AXIS));
-		window.setContentPane(parent);
-//		window.setVisible(true);
-		
-		Iterator<String> iterator = sourceText.iterator();
-		iterator.next();
-		String line2 = iterator.next();
-		
-		int displayWidth = (int) GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration().getBounds().getWidth();
-//		System.out.println("displayWidth=="+displayWidth);
-		Panel panel = new Panel(parent,line2.split(" "),spanLimit,translations);
-		int totalWidth = (int) panel.getPreferredSize().getWidth();
-//		System.out.println(panel.getPreferredSize());
-//		System.out.println(panel.getSize());
-//		window.pack();
-//		int totalWidth=panel.getWidth();
-		int extra=displayWidth*2/3;
-//		window.setVisible(false);
-		List<Panel> panels = new ArrayList<Panel>();
-		panels.add(panel);
-		for (int x=displayWidth; x+extra<totalWidth; x+=displayWidth) {
-//			System.out.println("x=="+x+"\ttotalWidth=="+totalWidth);
-			panel = new Panel(parent,line2.split(" "),spanLimit,translations);
-			panels.add(panel);
-		}
-		
-		window.pack();
-//		System.out.println(panel.getSize());
-		window.setSize((int) displayWidth, window.getHeight());
-//		System.out.println(panel.getSize());
-		
-//		System.out.println(panel.getSize());
-		
-		for (int x=displayWidth; x+extra<totalWidth; x+=displayWidth) {
-//			panel = new Panel(parent,line2.split(" "),spanLimit,translations);
-//			window.pack();
-//			window.setSize((int) displayWidth, window.getHeight());
-//			window.setVisible(true);
-			panels.get(x/displayWidth).scrollPane.getViewport().setViewPosition(new Point(x,0));
-		}
-		window.setVisible(true);
-//		
-//		
-//		Panel panel2 = new Panel(parent,line2.split(" "),spanLimit,translations);
-//		Panel panel3 = new Panel(parent,line2.split(" "),spanLimit,translations);
-//		Panel panel4 = new Panel(parent,line2.split(" "),spanLimit,translations);
-//
-//		
-//		
-//		System.err.println("Before:\t" + panel2.scrollPane.getViewport().getViewRect().getWidth());
-////		System.err.println("Visrec:\t" + panel2.scrollPane.getVisibleRect());
-//		
-////		panel2.scrollPane.scrollRectToVisible(new Rectangle(1276*2,0,1276*2+1,0));
-//		System.out.println(panel1.getSize());
-//		panel1.scrollPane.getViewport().setViewPosition(new Point(0,0));
-//		panel2.scrollPane.getViewport().setViewPosition(new Point(1276,0));
-//		panel3.scrollPane.getViewport().setViewPosition(new Point(1276*2,0));
-//		panel4.scrollPane.getViewport().setViewPosition(new Point(1276*3,0));
-//		
-////		System.err.println("After:\t" + panel2.scrollPane.getViewport().getViewRect());
-////		System.err.println("Visrec:\t" + panel2.scrollPane.getVisibleRect());
-		
-	}
+
 }
--- src/good/SentenceTranslationOptionsPanel.java
+++ src/good/SentenceTranslationOptionsPanel.java
@@ -2,6 +2,8 @@ package good;
 
 import java.awt.Dimension;
 import java.awt.GraphicsEnvironment;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Point;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,13 +12,18 @@ import java.util.Iterator;
 import java.util.List;
 
 import javax.swing.BoxLayout;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 
 @SuppressWarnings("serial")
 public class SentenceTranslationOptionsPanel extends JPanel {
 
 	private final int displayWidth;
+	private final int numRows;
 	
 	public SentenceTranslationOptionsPanel(String[] args, int spanLimit, TranslationOptions... translationsList) {
 		this(args,spanLimit,Arrays.asList(translationsList));
@@ -24,17 +31,19 @@ public class SentenceTranslationOptionsPanel extends JPanel {
 	
 	public SentenceTranslationOptionsPanel(String[] args, int spanLimit, List<TranslationOptions> translationsList) {
 		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		
+		this.numRows=Math.min(args.length, spanLimit);
 		this.displayWidth = getMaxDisplayWidth();
 		
-		Panel panel = new Panel(this,args,spanLimit,translationsList);
+		ChildScrollPane panel = new ChildScrollPane(args,spanLimit,translationsList);
 		int totalWidth = (int) panel.getPreferredSize().getWidth();
 		int extra=displayWidth*2/3;
-		List<Panel> panels = new ArrayList<Panel>();
+		List<ChildScrollPane> panels = new ArrayList<ChildScrollPane>();
 		panels.add(panel);
+		this.add(panel);
 		for (int x=displayWidth; x+extra<totalWidth; x+=displayWidth) {
-			panel = new Panel(this,args,spanLimit,translationsList);
+			panel = new ChildScrollPane(args,spanLimit,translationsList);
 			panels.add(panel);
+			this.add(panel);
 		}
 
 		Dimension size = new Dimension((int) displayWidth, (int) this.getPreferredSize().getHeight());
@@ -43,7 +52,7 @@ public class SentenceTranslationOptionsPanel extends JPanel {
 		this.setSize(size);
 
 		for (int x=displayWidth; x+extra<totalWidth; x+=displayWidth) {
-			panels.get(x/displayWidth).scrollPane.getViewport().setViewPosition(new Point(x,0));
+			panels.get(x/displayWidth).getViewport().setViewPosition(new Point(x,0));
 		}
 	}
 	
@@ -75,5 +84,93 @@ public class SentenceTranslationOptionsPanel extends JPanel {
 		window.setVisible(true);
 		
 		
+	}
+	
+	private class ChildScrollPane extends JScrollPane {
+		
+		ChildScrollPane(String[] args, int spanLimit, List<TranslationOptions> translationsList) {
+			super(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+			setViewportView(new ChildPanel(args,spanLimit,translationsList));
+		}
+		
+	}
+	
+	private class ChildPanel extends JPanel {
+
+		final JComboBox[][] comboBoxes;
+		
+		public ChildPanel(String[] args, int spanLimit, List<TranslationOptions> translationsList) {
+			super(new GridBagLayout());
+			
+			for (int i=0; i<args.length*2; i++) {
+				GridBagConstraints c = new GridBagConstraints();
+				c.gridx = i;
+				c.gridy = 0;
+				c.gridheight = 1;
+				c.gridwidth = 1;
+				c.weightx = 1;
+				c.fill = GridBagConstraints.HORIZONTAL;
+				JLabel label = new JLabel("");
+				this.add(label,c);
+			}
+
+			// Add words
+			for (int i=0; i<args.length; i+=1) {
+				GridBagConstraints c = new GridBagConstraints();
+				c.gridx = i*2;
+				c.gridy = 1;
+				c.gridheight = 1;
+				c.gridwidth = 2;
+				c.weightx = 1;
+				c.fill = GridBagConstraints.HORIZONTAL;
+
+				JLabel label = new JLabel(args[i], JLabel.CENTER);
+				this.add(label,c);
+				this.doLayout();
+			}
+			
+			
+			{
+				 
+				comboBoxes = new JComboBox[numRows][];
+
+				// Add rows to chart
+				for (int row=0; row<numRows; row+=1) {
+					
+					int numCells=args.length-row;
+					comboBoxes[row] = new JComboBox[numCells];
+					
+					// Add cells to chart
+					for (int cell=0; cell<numCells; cell+=1) {
+
+						GridBagConstraints c = new GridBagConstraints();
+						c.gridx = row + cell*2;
+						c.gridy = 2 + row;
+						c.gridheight = 1;
+						c.gridwidth = 2;
+						c.weightx = 1;
+						c.fill = GridBagConstraints.HORIZONTAL;
+
+						StringBuilder sourcePhraseBuilder = new StringBuilder();
+						for (int wordIndex=cell, lastIndex=cell+row; wordIndex<=lastIndex; wordIndex++) {
+							if (wordIndex>cell) {
+								sourcePhraseBuilder.append(' ');
+							}
+							sourcePhraseBuilder.append(args[wordIndex]);
+						}
+						Model model = new Model(sourcePhraseBuilder.toString(), translationsList);
+						JComboBox comboBox = new JComboBox(model);
+						comboBox.setEditable(true);
+						comboBoxes[row][cell] = comboBox;
+						this.add(comboBox,c);
+						this.doLayout();
+					}
+
+				}
+
+			}
+			
+		}
+
 	}
 }
