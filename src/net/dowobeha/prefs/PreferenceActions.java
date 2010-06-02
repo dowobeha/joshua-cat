f4ae934bf83c43336cf63093b66d91204d625f14
Lane Schwartz <dowobeha@gmail.com>
2010-06-02 16:03:16 +0000

    Working on preferences
    git-svn-id: https://joshua.svn.sourceforge.net/svnroot/joshua/branches/lane.scratch@1693 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

--- src/net/dowobeha/prefs/PreferenceActions.java
+++ src/net/dowobeha/prefs/PreferenceActions.java
@@ -1,23 +1,47 @@
 package net.dowobeha.prefs;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.prefs.Preferences;
 
 import javax.swing.KeyStroke;
 
+import com.google.common.collect.Multimap;
+import com.google.common.collect.HashMultimap;
+
 public class PreferenceActions  {
 
+	private final Preferences preferences;
+	private final Multimap<String,Runnable> actionMap;
 	
+	private final Map<String,String> defaults;
 	
-	public PreferenceActions() {
-		
+	public PreferenceActions(Class<?> c) {
+		this.preferences = Preferences.userNodeForPackage(c);
+		this.actionMap = HashMultimap.create();
+		this.defaults = new HashMap<String,String>();
 	}
 	
-	public void registerDefault(Class<?> c, String preferenceName, KeyStroke defaultKeystroke) {
-		Preferences.userNodeForPackage(c).put(preferenceName, defaultKeystroke.toString());
+	public void setDefaultKeystroke(String preferenceName, KeyStroke defaultKeystroke) {
+		defaults.put(preferenceName, defaultKeystroke.toString());
 	}
 	
-	public void register(Class<?> c, Runnable action, String... preferenceNames) {
-		
+	public void setKeystroke(String preferenceName, KeyStroke keystroke) {
+		preferences.put(preferenceName, keystroke.toString());
+		for (Runnable action : actionMap.get(preferenceName)) {
+			action.run();
+		}
+	}
+	
+	public KeyStroke getKeystroke(String preferenceName) {
+		return KeyStroke.getKeyStroke(preferences.get(preferenceName, defaults.get(preferenceName)));
+	}
+	
+	public void register(Runnable action, String... preferenceNames) {
+		for (String preferenceName : preferenceNames) {
+			actionMap.put(preferenceName, action);
+		}
+		action.run();
 	}
 
 }
